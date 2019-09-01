1d4cf7ec1d60d27560f8d8a0006dea398fface76
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2019-09-01 16:55:15 -0500

    added event listener to add a black, numbered square everytime the button is clicked.

--- scripts.js
+++ scripts.js
@@ -7,15 +7,18 @@ window.addEventListener('DOMContentLoaded', function () {
   document.body.appendChild(btn);
 });
 
-let counter = 1
+
+let counter = 1;
 
 btn.addEventListener('click', function () {
-  let div = document.createElement('div.square');
+  let div = document.createElement('div');
+  div.classList.add('square');
   let h2 = document.createElement('h2');
-  let h2Text = document.createTextNode(h2.id);
   h2.id = counter;
+  let h2Text = document.createTextNode(h2.id);
   counter++;
   h2.appendChild(h2Text);
   div.appendChild(h2);
-  
+  document.body.appendChild(div);
+  console.log(counter);
 })
