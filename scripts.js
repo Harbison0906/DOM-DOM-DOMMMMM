30b3b4b3ff0ffba34fd7aaf561e6596d7b1c9184
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2019-09-04 18:55:47 -0500

    added event to change background color of each square on click; styled h2s

--- scripts.js
+++ scripts.js
@@ -1,40 +1,48 @@
-let btn = document.createElement('button');
-let btnText = document.createTextNode('Add Square');
-
-
-btn.appendChild(btnText);
-
 window.addEventListener('DOMContentLoaded', function () {
+
+  let btn = document.createElement('button');
+  let btnText = document.createTextNode('Add Square');
+
+  btn.appendChild(btnText);
   document.body.appendChild(btn);
+
   let flexDiv = document.createElement('div');
   flexDiv.classList.add('container');
   document.body.appendChild(flexDiv);
 
+
+  let counter = 1;
+
+  btn.addEventListener('click', function () {
+    let div = document.createElement('div');
+    div.id = counter;
+    div.classList.add('square');
+    counter++;
+
+    flexDiv.appendChild(div);
+
+
+
+    div.addEventListener('mouseenter', function () {
+      let h2 = document.createElement('h2');
+      h2.id = counter;
+      let h2Text = document.createTextNode(div.id);
+      h2.appendChild(h2Text);
+      div.appendChild(h2);
+    })
+
+    div.addEventListener('mouseleave', function () {
+      var elem = document.getElementById(counter);
+      elem.parentNode.removeChild(elem);
+    })
+
+    let colorArray = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
+      '#E6B333', '#3366E6', '#999966'];
+
+    div.addEventListener('click', function () {
+      div.style.backgroundColor = colorArray[Math.floor(Math.random() * colorArray.length)];
+
+    });
+  });
+
 });
-
-
-
-let counter = 1;
-
-btn.addEventListener('click', function () {
-
-  let div = document.createElement('div');
-  div.id = counter;
-  div.classList.add('square');
-  counter++;
-  document.body.appendChild(div);
-
-  div.addEventListener('mouseenter', function () {
-    let h2 = document.createElement('h2');
-    h2.id = counter;
-    let h2Text = document.createTextNode(div.id);
-    h2.appendChild(h2Text);
-    div.appendChild(h2);
-  })
-
-  div.addEventListener('mouseleave', function () {
-    var elem = document.getElementById(counter);
-    elem.parentNode.removeChild(elem);
-  })
-});
-
