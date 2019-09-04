6841e22c329be32649df339eb6800355ac4964b8
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2019-09-03 21:36:54 -0500

    playing around with events to bring up the box number when hovering over box

--- scripts.js
+++ scripts.js
@@ -1,24 +1,40 @@
 let btn = document.createElement('button');
 let btnText = document.createTextNode('Add Square');
 
+
 btn.appendChild(btnText);
 
 window.addEventListener('DOMContentLoaded', function () {
   document.body.appendChild(btn);
+  let flexDiv = document.createElement('div');
+  flexDiv.classList.add('container');
+  document.body.appendChild(flexDiv);
+
 });
+
 
 
 let counter = 1;
 
 btn.addEventListener('click', function () {
+
   let div = document.createElement('div');
+  div.id = counter;
   div.classList.add('square');
-  let h2 = document.createElement('h2');
-  h2.id = counter;
-  let h2Text = document.createTextNode(h2.id);
   counter++;
-  h2.appendChild(h2Text);
-  div.appendChild(h2);
   document.body.appendChild(div);
-  console.log(counter);
-})
+
+  div.addEventListener('mouseenter', function () {
+    let h2 = document.createElement('h2');
+    h2.id = counter;
+    let h2Text = document.createTextNode(div.id);
+    h2.appendChild(h2Text);
+    div.appendChild(h2);
+  })
+
+  div.addEventListener('mouseleave', function () {
+    var elem = document.getElementById(counter);
+    elem.parentNode.removeChild(elem);
+  })
+});
+
